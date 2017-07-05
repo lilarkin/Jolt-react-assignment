b4a9ff53c7fab1b74c59409a718af0b2973ec57b
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-07-05 14:46:24 -0600

    makes notes and adds psuedo code

--- notes.md
+++ notes.md
@@ -1,3 +1,3 @@
 Start Time:  11a				End Time:
 
-Put any notes for us here.
+I can see that the typicode documentation is talking about implementing every step of the README.md file, however, I don't know how to implement it and localhost:3000 is saying that no server is running.  I cannot debug.
--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './Card.css';
 import ReactDom from 'react-dom';
-//need to bring in people api for props- GET people... where do I make this route?  
-//need to bring in planets api for props - GET planets... where do I make this route?
+// need to bring in people api for props- GET people
+// need to bring in planets api for props - GET planets... where do I make this request?
 
 class Card extends Component {
   constructor(props) {
@@ -11,7 +11,7 @@ class Card extends Component {
     this.state = {person: []}
   }
 
-//AJAX?????
+// AJAX?????
   // componentDidMount() {
   //   let root = 'http://localhost:3008';
 
@@ -36,27 +36,28 @@ class Card extends Component {
   }
 
   render() {
-    const persons = this.state.person.map((item, i) => {
+    const persons = this.state.person.map((person, i) => {
       return (
         <div className='card'>
           <div className='card-content'>
-              <div className='card-name'>{this.props.people.name}</div>
-              <img src={'http://localhost:3008/' + {this.props.persons.name} + '.jpg'} alt='profile'/>
-              <p>
+              <div className='card-name'>{person.name}</div>
+                <img src={'http://localhost:3008/' + {person.name} + '.jpg'} alt='profile'/>
+                <p>
                   <span>Birthday:</span>
-                  <span>{this.props.persons.birth_year}</span>
-              </p>
-              <p>
+                  <span>{person.birth_year}</span>
+                </p>
+                <p>
                   {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people
 
                   Need to associate id from people to corresponding id in planets */}
                   <span>Homeworld:</span>
                   <span>{this.props.planets.name}</span>
-              </p>
-          </div>
-      </div>  
+                </p>
+            </div>
+        </div>  
       )
     });
+    // pagination goes here?
   }
 }
 
